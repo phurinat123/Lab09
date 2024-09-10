8a7b2ff8864e2104531308b80ce52c396dd757cc
Sleepy7D <eumaeim@gmail.com>
2024-09-10 14:35:06 +0700

    UpdateFeature1

--- Lab09/Game1.cs
+++ Lab09/Game1.cs
@@ -9,6 +9,7 @@ namespace Lab09
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+//EimA
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
